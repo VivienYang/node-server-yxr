b0b55327e4ff938c5a954d8a5ac6b90d1bf8a42d
vivien_yang <1740768148@qq.com>
2023-10-08 14:46:16 +0800

    feat: 1.匹配public目录下的任意文件2.不存在的文件返回404页面3.默认index页面

--- index.ts
+++ index.ts
@@ -9,33 +9,40 @@ const server = http.createServer();
 server.on('request', (request: IncomingMessage, response: ServerResponse) => {
   let { method, url } = request;
   console.log(method, url);
+  const object = new URL(url || '', 'http://localhost');
+  let { pathname, search } = object;
+  console.log(search);
   if (method === 'GET') {
-    switch (url) {
-      case '/index.html':
-        fs.readFile(path.join(publicPath, 'index.html'), (err, data) => {
-          if (err) throw err;
-          response.setHeader('Content-type', 'text/html;charset:utf-8');
-          response.write(data.toString());
-          response.end();
-        });
-        break;
-      case '/style.css':
-        fs.readFile(path.join(publicPath, 'style.css'), (err, data) => {
-          if (err) throw err;
-          response.setHeader('Content-type', 'text/css;charset:utf-8');
-          response.write(data.toString());
-          response.end();
-        });
-        break;
-      case '/main.js':
-        fs.readFile(path.join(publicPath, 'main.js'), (err, data) => {
-          if (err) throw err;
-          response.setHeader('Content-type', 'text/javascript;charset:utf-8');
-          response.write(data.toString());
-          response.end();
-        });
-        break;
+    let fileName = pathname.slice(1);
+    if(fileName===''){
+      fileName='index.html' //默认返回index.html
     }
+    console.log('fileName--', fileName);
+    fs.readFile(path.join(publicPath, fileName), (err, data) => {
+      if (err) {
+        console.log(err)
+        if (err.errno === -2) {
+          response.statusCode = 404;
+          response.setHeader('Content-Type', 'text/html;charset=utf-8');
+          // 当找不到页面的时候，返回一个404页面
+          fs.readFile(path.join(publicPath, '404.html'),(err,data)=>{
+            if(err){
+              response.end('您请求的内容不存在呀，请再确认下~');
+            }else{
+              response.end(data)
+            }
+          })
+        } else {
+          response.statusCode = 500;
+          response.setHeader('Content-Type', 'text/plain;charset=utf-8');
+          response.end('服务器繁忙，请稍后再试~');
+        }
+      }else{
+        // response.setHeader('Content-type', 'text/html;charset:utf-8');
+        response.end(data);    
+      }
+    });
+
   }
 });
 
