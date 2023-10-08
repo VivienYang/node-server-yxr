cefd420f3d786f0e5c1981bf9767bb555cbaf540
vivien_yang <1740768148@qq.com>
2023-10-08 15:25:25 +0800

    添加缓存选项-Cache-Control

--- index.ts
+++ index.ts
@@ -8,41 +8,47 @@ const server = http.createServer();
 
 server.on('request', (request: IncomingMessage, response: ServerResponse) => {
   let { method, url } = request;
+  if (method !== 'GET') {
+    response.statusCode = 405;
+    response.end();
+    return;
+  }
   console.log(method, url);
   const object = new URL(url || '', 'http://localhost');
   let { pathname, search } = object;
   console.log(search);
   if (method === 'GET') {
     let fileName = pathname.slice(1);
-    if(fileName===''){
-      fileName='index.html' //默认返回index.html
+    if (fileName === '') {
+      fileName = 'index.html'; //默认返回index.html
     }
     console.log('fileName--', fileName);
     fs.readFile(path.join(publicPath, fileName), (err, data) => {
       if (err) {
-        console.log(err)
+        console.log(err);
         if (err.errno === -2) {
           response.statusCode = 404;
           response.setHeader('Content-Type', 'text/html;charset=utf-8');
           // 当找不到页面的时候，返回一个404页面
-          fs.readFile(path.join(publicPath, '404.html'),(err,data)=>{
-            if(err){
+          fs.readFile(path.join(publicPath, '404.html'), (err, data) => {
+            if (err) {
               response.end('您请求的内容不存在呀，请再确认下~');
-            }else{
-              response.end(data)
+            } else {
+              response.end(data);
             }
-          })
+          });
         } else {
           response.statusCode = 500;
           response.setHeader('Content-Type', 'text/plain;charset=utf-8');
           response.end('服务器繁忙，请稍后再试~');
         }
-      }else{
+      } else {
         // response.setHeader('Content-type', 'text/html;charset:utf-8');
-        response.end(data);    
+        let cacheAge = 3600 * 24 * 365;
+        response.setHeader('Cache-Control', `public, max-age=${cacheAge}`);
+        response.end(data);
       }
     });
-
   }
 });
 
